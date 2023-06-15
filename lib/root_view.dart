5060e7a6b6e5aca41d844707eb414a2889fcf42a
Sikandar Sadaqat <captainsikandar47@gmail.com>
2023-06-15 15:30:07 +0500

    added rotation animation to the text

--- lib/root_view.dart
+++ lib/root_view.dart
@@ -27,7 +27,10 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  bool _isRotated = false;
   Color color = Colors.white;
   final double opacity = 1.0;
   final random = math.Random();
@@ -37,13 +40,38 @@ class _MyHomePageState extends State<MyHomePage> {
     final red = random.nextInt(256);
     final green = random.nextInt(256);
     final blue = random.nextInt(256);
+
     setState(() {
       color = Color.fromRGBO(red, green, blue, opacity);
     });
+    _animateTextRotation();
+  }
+
+  void _animateTextRotation() {
+    if (_isRotated) {
+      _animationController.reverse();
+    } else {
+      _animationController.forward();
+    }
+    _isRotated = !_isRotated;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 500),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
+    final Animation<double> _animation = CurvedAnimation(
+      parent: _animationController,
+      curve: Curves.easeInOut,
+    );
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Random Color Generator'),
@@ -56,9 +84,18 @@ class _MyHomePageState extends State<MyHomePage> {
           alignment: Alignment.center,
           width: double.infinity,
           color: color,
-          child: const Text('Hello there'),
+          child: RotationTransition(
+            turns: _animation,
+            child: const Text('Hello there'),
+          ),
         ),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
   }
 }
